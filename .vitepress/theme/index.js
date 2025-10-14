faecb0abe09b94e15969e836f28b1c031b665d7f
Md Kamrul Islam <kevin@wpmanageninja.com>
2025-10-14 18:11:47 +0600

    deleted all

--- .vitepress/theme/index.js
+++
@@ -1,9 +0,0 @@
-import DefaultTheme from 'vitepress/theme';
-import GlossaryTooltip from 'vitepress-plugin-glossary/vue';
-
-export default {
-    ...DefaultTheme,
-    enhanceApp({ app }) {
-        app.component('GlossaryTooltip', GlossaryTooltip);
-    }
-};
